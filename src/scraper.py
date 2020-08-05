057a655db1730f1d5a2b16c49481c7f1049888b3
Max Wieduwilt <maxwieduwilt@utexas.edu>
2020-08-05 13:51:33 -0500

    stream_content throws exception when .yaml is invalid

--- src/scraper.py
+++ src/scraper.py
@@ -1,9 +1,9 @@
 import picker
 import praw
-import prawcore
 import yaml
 import io
 import logging
+import prawcore.exceptions
 
 class ContentScraper:
     def __init__(self):
@@ -11,7 +11,6 @@ class ContentScraper:
         self.subs_and_keywords = self.parse_subs_and_keywords()
         self.content_filter = picker.ContentPicker(self.subs_and_keywords)
         
-
     def parse_subs_and_keywords(self):
         # load in yaml file containing desired subs and keywords
         with open("../content.yaml", 'r') as stream:
@@ -21,8 +20,8 @@ class ContentScraper:
     def validate_sub_names(self):
         subs_are_valid = True
         sub_names = self.subs_and_keywords.keys()
+        # run through each subreddit name, catching exceptions indicating invalid subreddit
         for sub_name in sub_names:
-            print(sub_name)
             try:
                 sub_id = self.reddit.subreddit(sub_name).id
             except prawcore.exceptions.Redirect:
@@ -31,6 +30,10 @@ class ContentScraper:
         return subs_are_valid
 
     def stream_content(self):
+        # validate subreddits in content.yaml before streaming
+        if not self.validate_sub_names():
+            raise ValueError("There is an invalid subreddit name in content.yaml")
+
         # use streams for submissions, search somehow
         subs_as_string = "+".join(self.subs_and_keywords.keys())
         for submission in self.reddit.subreddit(subs_as_string).stream.submissions():
@@ -40,4 +43,4 @@ class ContentScraper:
 
 if __name__ == "__main__":
     scraper = ContentScraper()
-    print(scraper.validate_sub_names());
+    scraper.stream_content()
