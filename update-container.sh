74b203c4dc38ffaf38931a21a2f0f2fa56a778dc
Max Wieduwilt <maxwieduwilt@utexas.edu>
2020-08-18 20:38:24 -0500

    added descriptive echo output in shell script

--- update-container.sh
+++ update-container.sh
@@ -9,10 +9,15 @@ then
 fi
 
 # remove existing resources
+echo "Stopping old container..."
 docker stop ${CONTAINER_NAME}
+echo "Removing old container..."
 docker container rm ${CONTAINER_NAME}
+echo "Removing old image..."
 docker image rm ${IMAGE_NAME}
 
 # start up container with updated code
+echo "Building new image..."
 docker build -t ${IMAGE_NAME} .
+echo "Starting new container..."
 docker run -dp 1544:1544 ${IMAGE_NAME}
