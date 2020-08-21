417e794647d3e6b9ac1cf6eca1197f9fd576ae95
Max Wieduwilt <maxwieduwilt@utexas.edu>
2020-08-21 16:27:36 -0500

    added basic methods for checking whether db container submission and clearing submission list

--- src/storage.py
+++ src/storage.py
@@ -1,21 +1,26 @@
+import pymongo
 from pymongo import MongoClient
 from pprint import pprint
 
 class VisitedSubmissionList:
     def __init__(self): 
-        client = MongoClient("mongodb://localhost:27017")
-        self.db = client.content_prospector
+        self.client = MongoClient("mongodb://localhost:27017")
+        self.db = self.client.content_prospector
     
     def insert_submission_id(self, submission_id):
-        self.db.submission_ids.insert_one({submission_id : submission_id})
+        self.db.submission_ids.insert_one({ "id" : submission_id })
 
     def check_submission_id(self, submission_id):
-        return None
+        if self.db.submission_ids.find({ "id" : submission_id }).count() > 0:
+            return True
+        return False
 
     def clear_submission_list(self):
-        return None
+        self.client.drop_database('content_prospector')
 
 
 if __name__ == "__main__":
     list = VisitedSubmissionList()
     list.insert_submission_id("345272")
+    print(list.check_submission_id("345272"))
+    list.clear_submission_list()
